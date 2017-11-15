d458a16365b4148c9f71b25adef7cf4638d23875
guangjun <yangguangjun123@gmail.com>
2017-11-15 18:47:37 +0000

    Changes for test class

--- src/test/java/org/myproject/ecommerce/ProductCatalogServiceTest.java
+++ src/test/java/org/myproject/ecommerce/ProductCatalogServiceTest.java
@@ -1,4 +1,8 @@
 package org.myproject.ecommerce;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 public class ProductCatalogServiceTest {
 }
